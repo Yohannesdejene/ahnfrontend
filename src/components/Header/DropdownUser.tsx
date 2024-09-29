958e43cfc04db282e785917689e0f48c747d0ff1
Yohannesdejene <yohannesdejene23@gmail.com>
2024-09-29 07:11:12 +0300

    logout added

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import { LinearProgress } from "@mui/material";
 import ClickOutside from "@/components/ClickOutside";
-
+import { deleteSessionKeys } from "@/utils/sessionManager";
 const DropdownUser = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [logoutLoading, setLoutLoading] = useState(false);
+  const router = useRouter();
+  const handleLogout = () => {
+    setLoutLoading(true);
+    deleteSessionKeys();
 
+    router.push("/auth/login");
+    setTimeout(() => {
+      setLoutLoading(false);
+    }, 6000);
+  };
   return (
     <ClickOutside onClick={() => setDropdownOpen(false)} className="relative">
       <Link
@@ -128,7 +140,10 @@ const DropdownUser = () => {
               </Link>
             </li>
           </ul>
-          <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+          <button
+            className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+            onClick={handleLogout}
+          >
             <svg
               className="fill-current"
               width="22"
@@ -146,7 +161,7 @@ const DropdownUser = () => {
                 fill=""
               />
             </svg>
-            Log Out
+            {logoutLoading ? <div>Loading... </div> : <div> Log Out</div>}
           </button>
         </div>
       )}
