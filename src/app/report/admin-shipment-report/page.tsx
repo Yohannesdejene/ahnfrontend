8f49175b20e249bba18661a02bc3d02f87052119
Yohannesdejene <yohannesdejene23@gmail.com>
2025-03-20 16:20:41 +0300

    push clean new dev

--- src/app/report/admin-shipment-report/page.tsx
+++ src/app/report/admin-shipment-report/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Metadata } from "next";
 import AdminShipmentReport from "@/components/report/adminShipmentReport";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import { AuthProvider } from "@/context/AuthContext"; // path as necessary
+import { AuthProvider } from "@/context/AuthContext"; //s necessary
 
 export const metadata: Metadata = {
   title: "Admin shipment report",
